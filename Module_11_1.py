fafb90b93da71d4bb61f1769bac0d23f16d0af21
Abeille1990 <tatiana.tyukova@gmail.com>
2024-10-14 19:38:22 +0300

    Update Module_11_1.py
    token changed for confidentiality

--- Module_11_1.py
+++ Module_11_1.py
@@ -6,13 +6,13 @@ print('Enter release year(1960 to 2024):')
 y = input()
 
 
-ACCESS_TOKEN = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMTEyYjdjYzIxMzRjNjgzZTNjNjA5YTQyNWVjZjJlYiIsIm5iZiI6MTcyODgyNzYzNS44MDE4ODMsInN1YiI6IjY3MGJjYjJlMzdkODZkNTIwYmIwZWE2NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Kod83hWWmcid2yUxIkKnmkEDzx45vZedzqhiuxOzis0"
+ACCESS_TOKEN = "Bearero eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMTEyYjdjYzIxMzRjNjgzZTNjNjA5YTQyNWVjZjJlYiIsIm5iZiI6MTcyODgyNzYzNS44MDE4ODMsInN1YiI6IjY3MGJjYjJlMzdkODZkNTIwYmIwZWE2NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Kod83hWWmcid2yUxIkKnmkEDzx45vZedzqhiuxOzis0"
 API_URL = 'https://api.themoviedb.org/3/search/movie'
 TMDB_URL = 'https://www.themoviedb.org'
 
 headers = {
     "accept": "application/json",
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMTEyYjdjYzIxMzRjNjgzZTNjNjA5YTQyNWVjZjJlYiIsIm5iZiI6MTcyO"
+    "Authorization": "Bearero eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMTEyYjdjYzIxMzRjNjgzZTNjNjA5YTQyNWVjZjJlYiIsIm5iZiI6MTcyO"
                      "DgyNzYzNS44MDE4ODMsInN1YiI6IjY3MGJjYjJlMzdkODZkNTIwYmIwZWE2NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZX"
                      "JzaW9uIjoxfQ.Kod83hWWmcid2yUxIkKnmkEDzx45vZedzqhiuxOzis0"
 }
